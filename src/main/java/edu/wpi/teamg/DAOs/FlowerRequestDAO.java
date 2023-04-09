e519a6fe2ad10151e778740109c732e393c746ea
da-munchies24 <donbothermeatall16@gmail.com>
2023-04-09 18:48:47 -0400

    Update FlowerDAO
    made the insert and delete methods for the FlowerDAO and made it so its reflected in the hashmap

--- src/main/java/edu/wpi/teamg/DAOs/FlowerRequestDAO.java
+++ src/main/java/edu/wpi/teamg/DAOs/FlowerRequestDAO.java
@@ -18,13 +18,15 @@ public class FlowerRequestDAO implements DAO {
   private String SQL_flowerRequest;
   private String SQL_Request;
 
+  NodeDAO nodeDAO = new NodeDAO();
+
   @Override
   public HashMap<Integer, FlowerRequest> getAll() throws SQLException {
     db.setConnection();
     PreparedStatement ps;
     ResultSet rs = null;
     SQL_flowerRequest =
-        "select * from iteration1.request join iteration1.flowerrequest on iteration1.request.reid= iteration1.flowerrequest.reqid";
+        "select * from iteration1.request join iteration1.flowerrequest on iteration1.request.reqid= iteration1.flowerrequest.reqid";
     try {
       ps = db.getConnection().prepareStatement(SQL_flowerRequest);
       rs = ps.executeQuery();
@@ -89,14 +91,64 @@ public class FlowerRequestDAO implements DAO {
       maxID = rs.getInt("reqID");
       maxID++;
     }
+
+    SQL_flowerRequest = "insert into teamgdb.iteration1.flowerrequest(reqid, flowertype, numflower, recipient, note) values (?,?,?,?,?)";
+    SQL_Request = "insert into teamgdb.iteration1.request(reqid,reqtype,empid,location, serveBy, status, requestdate, requesttime) values (?,?,?,?,?,?,?,?)";
+
+    try{
+      ps_Req = db.getConnection().prepareStatement(SQL_Request);
+      ps_Req.setInt(1,maxID);
+      ps_Req.setString(2,"FR");
+      ps_Req.setInt(3,((FlowerRequest)obj).getEmpid());
+
+      int nodeID = nodeDAO.getNodeIDbyLongName(((FlowerRequest)obj).getLocation());
+
+      ps_Req.setInt(4,nodeID);
+      ps_Req.setInt(5,((FlowerRequest) obj).getServeBy());
+      ps_Req.setObject(6,((FlowerRequest)obj).getStatus(),java.sql.Types.OTHER);
+      ps_Req.setDate(7,((FlowerRequest)obj).getRequestDate());
+      ps_Req.setTime(8,((FlowerRequest)obj).getRequestTime());
+      ps_Req.executeUpdate();
+
+      ps_getFlowerReq = db.getConnection().prepareStatement(SQL_flowerRequest);
+      ps_getFlowerReq.setInt(1,maxID);
+      ps_getFlowerReq.setString(2,((FlowerRequest)obj).getFlowerType());
+      ps_getFlowerReq.setInt(3,((FlowerRequest)obj).getNumFlower());
+      ps_getFlowerReq.setString(4,((FlowerRequest)obj).getRecipient());
+      ps_getFlowerReq.setString(5,((FlowerRequest)obj).getNote());
+
+      ps_getFlowerReq.executeUpdate();
+    }catch (SQLException e){
+      System.err.println("SQL Exception");
+      e.printStackTrace();
+    }
+    flowerHashMap.put(((FlowerRequest)obj).getReqid(),(FlowerRequest) obj);
+    db.closeConnection();
   }
 
   @Override
   public void delete(Object obj) throws SQLException {
     db.setConnection();
-    PreparedStatement ps_flowerRequest;
+    PreparedStatement ps_flower;
+    PreparedStatement ps_Request;
+    FlowerRequest flow= (FlowerRequest) obj;
 
+    SQL_flowerRequest="delete from teamgdb.iteration1.flowerrequest where reqid=?";
+    SQL_Request="delete from iteration1.flowerrequest where reqid=?";
+    try{
+      ps_flower=db.getConnection().prepareStatement(SQL_flowerRequest);
+      ps_flower.setInt(1,flow.getReqid());
+      ps_flower.executeUpdate();
 
+      ps_Request=db.getConnection().prepareStatement(SQL_Request);
+      ps_Request.setInt(1,flow.getReqid());
+      ps_Request.executeUpdate();
+    }catch(SQLException e){
+      System.err.println("SQL Exception");
+      e.printStackTrace();
+    }
+    flowerHashMap.remove(flow.getReqid());
+            db.closeConnection();
 
   }
 }
