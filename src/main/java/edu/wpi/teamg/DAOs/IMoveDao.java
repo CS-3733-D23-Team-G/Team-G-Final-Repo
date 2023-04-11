879eea4bf7549e6d681a9325d81986427665d3b8
Thomas3737 <thomas.mcdonagh037@gmail.com>
2023-04-10 23:46:02 -0400

    Addendum to previous commit
    Forgot to mention I worked together with Hung and Raj.

--- src/main/java/edu/wpi/teamg/DAOs/IMoveDao.java
+++ src/main/java/edu/wpi/teamg/DAOs/IMoveDao.java
@@ -3,7 +3,7 @@ package edu.wpi.teamg.DAOs;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface IMoveDao<T,K> {
+public interface IMoveDao<T, K> {
   public List<T> getAll() throws SQLException;
 
   public void update(T obj, String colName, K obj2) throws SQLException;
