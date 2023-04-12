c4f3c9e1ea7474e9df4245b2412384bd33b11692
Thomas3737 <thomas.mcdonagh037@gmail.com>
2023-04-11 21:41:51 -0400

    MAP EDITOR FOR THE NODES ON THE MAP FINALLY COMPLETE :)

--- src/main/java/edu/wpi/teamg/controllers/editPopUpController.java
+++ src/main/java/edu/wpi/teamg/controllers/editPopUpController.java
@@ -1,6 +1,8 @@
 package edu.wpi.teamg.controllers;
 
+import edu.wpi.teamg.DAOs.NodeDAO;
 import edu.wpi.teamg.ORMClasses.Node;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 
@@ -11,6 +13,7 @@ public class editPopUpController {
   @FXML MFXTextField nYcoord;
   @FXML MFXTextField nFloor;
   @FXML MFXTextField nBuilding;
+  @FXML MFXButton submit;
 
   public void initialize() {
 
@@ -19,6 +22,7 @@ public class editPopUpController {
     nYcoord.setEditable(true);
     nFloor.setEditable(true);
     nBuilding.setEditable(true);
+    submit.setOnMouseClicked(event -> editPopUp());
   }
 
   public void setFields(Node node) {
@@ -27,5 +31,20 @@ public class editPopUpController {
     nYcoord.setText(String.valueOf(node.getYcoord()));
     nFloor.setText(node.getFloor());
     nBuilding.setText(node.getBuilding());
+  }
+
+  public void editPopUp() {
+    NodeDAO nodeDAO = new NodeDAO();
+    Node node =
+        new Node(
+            Integer.parseInt(nID.getText()),
+            Integer.parseInt(nXcoord.getText()),
+            Integer.parseInt(nYcoord.getText()),
+            nFloor.getText(),
+            nBuilding.getText());
+    nodeDAO.update(node, "xcoord", Integer.parseInt(nXcoord.getText()));
+    nodeDAO.update(node, "ycoord", Integer.parseInt(nYcoord.getText()));
+    nodeDAO.update(node, "floor", nFloor.getText());
+    nodeDAO.update(node, "building", nBuilding.getText());
   }
 }
