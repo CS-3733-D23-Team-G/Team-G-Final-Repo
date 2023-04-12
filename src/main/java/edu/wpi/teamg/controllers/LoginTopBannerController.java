5604974521609da189e157cbf4facb04d84d2ae0
Sebastian Baldini <sebastian.baldini@outlook.com>
2023-04-11 21:58:20 -0400

    Login and patient banner created

---
+++ src/main/java/edu/wpi/teamg/controllers/LoginTopBannerController.java
@@ -0,0 +1,35 @@
+package edu.wpi.teamg.controllers;
+
+import edu.wpi.teamg.navigation.Navigation;
+import edu.wpi.teamg.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class LoginTopBannerController {
+
+  @FXML MFXButton exitButton;
+
+
+  ObservableList<String> list =
+      FXCollections.observableArrayList(
+          "Conference Room Request Form",
+          "Flowers Request Form",
+          "Furniture Request Form",
+          "Meal Request Form",
+          "Office Supplies Request Form");
+
+  @FXML
+  public void initialize() {
+    exitButton.setOnMouseClicked(event -> exit());
+  }
+
+  public void exit() {
+    Platform.exit();
+  }
+
+  
+}
