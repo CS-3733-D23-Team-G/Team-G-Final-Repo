4c746fcfd2861938e92b5756a4442d9ad53f92d1
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-13 06:13:10 -0400

    Created Office Supply getAll
    created a simple getAll for the officesupply service request dao, can be changed later when attributes change.

--- src/main/java/edu/wpi/teamg/DAOs/OfficeSupplyRequestDAO.java
+++ src/main/java/edu/wpi/teamg/DAOs/OfficeSupplyRequestDAO.java
@@ -1,12 +1,73 @@
 package edu.wpi.teamg.DAOs;
 
-import java.sql.SQLException;
+import edu.wpi.teamg.DBConnection;
+import edu.wpi.teamg.ORMClasses.OfficeSupplyRequest;
+import edu.wpi.teamg.ORMClasses.StatusTypeEnum;
+
+import java.sql.*;
 import java.util.HashMap;
 
 public class OfficeSupplyRequestDAO implements DAO{
+    HashMap<Integer, OfficeSupplyRequest> officeSupplyHash = new HashMap<>();
+    private static DBConnection db = new DBConnection();
+    private String SQL_maxID;
+    private String SQL_OfficeSupplyRequest;
+    private String SQL_Request;
+    NodeDAO nodeDAO = new NodeDAO();
     @Override
     public HashMap getAll() throws SQLException {
-        return null;
+        db.setConnection();
+        PreparedStatement ps;
+        ResultSet rs = null;
+        SQL_OfficeSupplyRequest =
+                "select * from iteration2.request join iteration2.officesupplyrequest on iteration2.request.reqid = iteration2.officesupplyrequest.reqid";
+
+        try{
+            ps = db.getConnection().prepareStatement(SQL_OfficeSupplyRequest);
+            rs=ps.executeQuery();
+        }catch (SQLException e){
+            System.err.println("SQL Exception");
+            e.printStackTrace();
+        }
+
+        while(rs.next()){
+            int reqid = rs.getInt("reqid");
+            String reqType = rs.getString("reqtype");
+            int empID  = rs.getInt("empid");
+            int location = rs.getInt("location");
+
+            HashMap longNameHash = new HashMap<>();
+            longNameHash=nodeDAO.getMandFLLongName();
+
+            String lName = (String)longNameHash.get(location);
+            int servBy = rs.getInt("serveby");
+            StatusTypeEnum status = StatusTypeEnum.valueOf(rs.getString("status"));
+            String recipient = rs.getString("recipient");
+            Date requestdate = rs.getDate("requestdate");
+            Time requesttime = rs.getTime("requesttime");
+            String supplyType = rs.getString("officesupplytype");
+            String note = rs.getString("note");
+            int numSupply = rs.getInt("numsupply");
+
+            OfficeSupplyRequest officeSupplyRequest =
+                    new OfficeSupplyRequest(
+                            reqType,
+                            empID,
+                            lName,
+                            servBy,
+                            status,
+                            requestdate,
+                            requesttime,
+                            supplyType,
+                            numSupply,
+                            recipient,
+                            note);
+            officeSupplyRequest.setReqid(reqid);
+            officeSupplyHash.put(reqid,officeSupplyRequest);
+        }
+
+        db.closeConnection();
+        return officeSupplyHash;
     }
 
     @Override
@@ -26,6 +87,6 @@ public class OfficeSupplyRequestDAO implements DAO{
 
     @Override
     public String getTable() {
-        return null;
+        return "teamgdb.iteration2.officesupplyrequest";
     }
 }
