5722c5cfb772784b8ab21d0a7339e541733f0440
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-13 05:20:13 -0400

    Basic DAO for office supply
    Starting of the office supply dao, realizing i forgot to pull in most recent version of dev branch.

---
+++ src/main/java/edu/wpi/teamg/DAOs/OfficeSupplyRequestDAO.java
@@ -0,0 +1,31 @@
+package edu.wpi.teamg.DAOs;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class OfficeSupplyRequestDAO implements DAO{
+    @Override
+    public HashMap getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void update(Object obj, String colName, Object obj2) throws SQLException {
+
+    }
+
+    @Override
+    public void insert(Object obj) throws SQLException {
+
+    }
+
+    @Override
+    public void delete(Object obj) throws SQLException {
+
+    }
+
+    @Override
+    public String getTable() {
+        return null;
+    }
+}
