421e4136e25fc7e22bc94965b3c701f380a8ac91
arsimonini <arsimonini@wpi.edu>
2023-04-17 03:22:44 -0400

    Finalized Most of the Signage Page and Editor
    Everything is working near perfectly with the exception of one part, the signage page will display nothing until something in the editor is clicked. Even when feeding in code from the editor, it will not display anything. I will have to try more tomorrow. Otherwise though, it works.

--- src/main/java/edu/wpi/teamg/controllers/SignageScreenSaverController.java
+++ src/main/java/edu/wpi/teamg/controllers/SignageScreenSaverController.java
@@ -1,45 +1,118 @@
 package edu.wpi.teamg.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 public class SignageScreenSaverController {
 
-  @FXML ImageView westArrow;
-  @FXML ImageView northArrow;
-  @FXML ImageView southArrow;
-  @FXML ImageView eastArrow;
+  //  @FXML ImageView westArrow;
+  //  @FXML ImageView northArrow;
+  //  @FXML ImageView southArrow;
+  //  @FXML ImageView eastArrow;
+
+  Image westArrow = new Image("edu/wpi/teamg/Images/WestArrow.png");
+  Image eastArrow = new Image("edu/wpi/teamg/Images/EastArrow.png");
+  Image southArrow = new Image("edu/wpi/teamg/Images/SouthArrow.png");
+  Image northArrow = new Image("edu/wpi/teamg/Images/NorthArrow.png");
+  Image noArrow = new Image("edu/wpi/teamg/Images/NoArrow.png");
+
+  @FXML ImageView arrow1 = new ImageView(noArrow);
+  @FXML ImageView arrow2 = new ImageView(noArrow);
+  @FXML ImageView arrow3 = new ImageView(noArrow);
+  @FXML ImageView arrow4 = new ImageView(noArrow);
+  @FXML ImageView arrow5 = new ImageView(noArrow);
+  @FXML ImageView arrow6 = new ImageView(noArrow);
+  @FXML ImageView arrow7 = new ImageView(noArrow);
+  @FXML ImageView arrow8 = new ImageView(noArrow);
+  @FXML ImageView arrow9 = new ImageView(noArrow);
+  @FXML ImageView arrow10 = new ImageView(noArrow);
+
+  @FXML Text nameLabel1;
+  @FXML Text nameLabel2;
+  @FXML Text nameLabel3;
+  @FXML Text nameLabel4;
+  @FXML Text nameLabel5;
+  @FXML Text nameLabel6;
+  @FXML Text nameLabel7;
+  @FXML Text nameLabel8;
+  @FXML Text nameLabel9;
+  @FXML Text nameLabel10;
+
+  ArrayList<ImageView> arrows;
 
   public void initialize() {
+
+    arrows =
+        new ArrayList<>(
+            Arrays.asList(
+                arrow1, arrow2, arrow3, arrow4, arrow5, arrow6, arrow7, arrow8, arrow9, arrow10));
+    //    Platform.runLater(
+    //            new Runnable() {
+    //              @Override
+    //              public void run() {
+    //                arrowSetUp(arrowDirectionNumber);
+    //              }
+    //            });
+
+
+    //HELP, I NEED SOMEBODY
+//    arrow2.setImage(westArrow);
+//    nameLabel2.setText("75 Lobby");
+//    arrow3.setImage(northArrow);
+//    nameLabel3.setText("Bathroom 75 Lobby");
+//    arrow7.setImage(eastArrow);
+//    nameLabel7.setText("Garden Cafe");
+//    arrow8.setImage(eastArrow);
+//    nameLabel8.setText("75 Lobby Information Desk");
+
     getArrowDirectionFromEditor();
+    getSavedNamesFromEditor();
   }
 
   public void getArrowDirectionFromEditor() {
     int[] arrowDirection;
     SignageEditorController controller = new SignageEditorController();
     arrowDirection = controller.getArrowDirection();
+    for (int i = 0; i <= 9; i++) {
 
-    if (arrowDirection[0] == 1) {
-      westArrow.setVisible(true);
+      switch (arrowDirection[i]) {
+        case 0:
+          arrows.get(i).setImage(null);
+          break;
+        case 1:
+          arrows.get(i).setImage(westArrow);
+          break;
+        case 2:
+          arrows.get(i).setImage(northArrow);
+          break;
+        case 3:
+          arrows.get(i).setImage(eastArrow);
+          break;
+        case 4:
+          arrows.get(i).setImage(southArrow);
+          break;
+      }
     }
+  }
 
-    if (arrowDirection[0] == 3) {
-      eastArrow.setVisible(true);
-    }
+  public void getSavedNamesFromEditor() {
+    String[] savedNames;
+    SignageEditorController controller = new SignageEditorController();
+    savedNames = controller.getSavedNames();
 
-    if (arrowDirection[0] == 2) {
-      northArrow.setVisible(true);
-    }
-
-    if (arrowDirection[0] == 4) {
-      southArrow.setVisible(true);
-    }
-
-    if (arrowDirection[0] == 0) {
-      westArrow.setVisible(false);
-      southArrow.setVisible(false);
-      northArrow.setVisible(false);
-      eastArrow.setVisible(false);
-    }
+    nameLabel1.setText(savedNames[0]);
+    nameLabel2.setText(savedNames[1]);
+    nameLabel3.setText(savedNames[2]);
+    nameLabel4.setText(savedNames[3]);
+    nameLabel5.setText(savedNames[4]);
+    nameLabel6.setText(savedNames[5]);
+    nameLabel7.setText(savedNames[6]);
+    nameLabel8.setText(savedNames[7]);
+    nameLabel9.setText(savedNames[8]);
+    nameLabel10.setText(savedNames[9]);
   }
 }
