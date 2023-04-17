fe00369201991d667b59e1ba97f682251b341d6b
21mara <80219659+21mara@users.noreply.github.com>
2023-04-16 23:20:15 -0400

    started working on storing hashed passwords

--- src/main/java/edu/wpi/teamg/controllers/LoginPageController.java
+++ src/main/java/edu/wpi/teamg/controllers/LoginPageController.java
@@ -10,7 +10,15 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
-public class LoginPageController {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class LoginPageController extends Node {
 
   @FXML private TextField emailBox;
 
@@ -42,6 +50,44 @@ public class LoginPageController {
     signUpButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     // If email or password is incorrct, set label text to " Email or Password is Incorrect. Please
     // try again.
+
+  }
+
+  public void start(Stage primaryStage) throws Exception {
+    // text field
+    // set title for the stage
+    primaryStage.setTitle("creating TextField");
+
+    // create a textfield
+    LoginPageController node = new LoginPageController();
+    //node.setLayoutX(100);
+    //node.setLayoutY(200);
+
+
+    // create a stack pane
+    StackPane r = new StackPane();
+
+    // add textfield
+    r.getChildren().add(node);
+
+    // create a scene
+    Scene sc = new Scene(r, 200, 200);
+
+    // set the scene
+    primaryStage.setScene(sc);
+
+    primaryStage.show();
+
+    // password field
+    primaryStage.setTitle("PasswordField Experiment 1");
+
+    PasswordField passwordField = new PasswordField();
+
+    HBox hbox = new HBox(passwordField);
+
+    Scene scene = new Scene(hbox, 200, 100);
+    primaryStage.setScene(scene);
+    primaryStage.show();
 
   }
 
