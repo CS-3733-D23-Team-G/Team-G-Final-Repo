6fe391ef6c14ec659c531d0976ebeeb98cd790ac
Thomas3737 <thomas.mcdonagh037@gmail.com>
2023-04-17 14:23:56 -0400

    Raj and I worked on the password functionality with the front end. Raj worked on the login Check feature while I handel the Incorrect username and password feature. We are going to add accounts but what we have is semi functional

--- src/main/java/edu/wpi/teamg/controllers/LoginController.java
+++ src/main/java/edu/wpi/teamg/controllers/LoginController.java
@@ -1,31 +1,83 @@
 package edu.wpi.teamg.controllers;
 
-import edu.wpi.teamg.navigation.Navigation;
-import edu.wpi.teamg.navigation.Screen;
+import static javafx.scene.paint.Color.RED;
+
+import edu.wpi.teamg.DAOs.AccountDAO;
+import edu.wpi.teamg.DBConnection;
+import edu.wpi.teamg.ORMClasses.Account;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import java.awt.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import org.controlsfx.control.textfield.CustomTextField;
 
 public class LoginController {
   @FXML MFXButton loginButton;
-  @FXML CustomTextField EmployeeIDs;
-  @FXML CustomTextField PasswordInput;
+  @FXML CustomTextField username;
+  @FXML CustomTextField password;
 
-  
+  @FXML Label userInc;
+
+  @FXML Label passInc;
+
+
+
+  DBConnection db = new DBConnection();
+  String query;
 
   public void initialize() {
+    userInc.setVisible(false);
+    passInc.setVisible(false);
     loginButton.setOnAction(event -> loginCheck());
   }
 
   public void loginCheck() {
-    String IDCheck = EmployeeIDs.getText();
-    String PasswordCheck = PasswordInput.getText();
-    if (IDCheck.equals("69420") && PasswordCheck.equals("yourtellingmeashrimpfriedthisrice?")) {
-      Navigation.navigate(Screen.HOME);
-      System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah");
-    } else {
-      System.out.println("naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah");
-      PasswordInput.setText("Incorrect Username or Password");
+    String user = username.getText(); // the fxid is still employeeid
+    String pass = password.getText();
+
+    AccountDAO accountDAO = new AccountDAO();
+    db.setConnection();
+    query = "select from " + accountDAO.getTable() + " where username = ?";
+    try {
+      PreparedStatement ps = db.getConnection().prepareStatement(query);
+      ResultSet rs = ps.executeQuery();
+      String tableUser = "";
+      int tableEmp = 0;
+      String tablePass = "";
+      byte[] tableSalt = null;
+      boolean tableAdmin = false;
+
+      while (rs.next()) {
+        tableUser = rs.getString("username");
+        tableEmp = rs.getInt("empid");
+        tablePass = rs.getString("hashPassword");
+        tableSalt = rs.getBytes("salt");
+        tableAdmin = rs.getBoolean("is_admin");
+      }
+
+      Account account = new Account();
+      account.setPassword(pass);
+
+      if (account.getHashedPassword(tableSalt).equals(tablePass)) {
+
+      } else {
+        incorrectPassword();
+      }
+
+    } catch (SQLException e) {
+      incorrectPassword();
+      System.err.println("SQL Exception on Account");
+      e.printStackTrace();
     }
+  }
+
+  public void incorrectPassword() {
+    userInc.setVisible(true);
+    passInc.setVisible(true);
+    userInc.setTextFill(RED);
+    passInc.setTextFill(RED);
   }
 }
