8994c35399c6c0151a5bfe28c72b6222ebe92c3c
arsimonini <arsimonini@wpi.edu>
2023-04-18 02:59:17 -0400

    Even MORE bugs for UI Fixed
    Logos for Login and Pathfinding Guest Page now send back.

--- src/main/java/edu/wpi/teamg/controllers/LoginTopBannerController.java
+++ src/main/java/edu/wpi/teamg/controllers/LoginTopBannerController.java
@@ -1,5 +1,7 @@
 package edu.wpi.teamg.controllers;
 
+import edu.wpi.teamg.navigation.Navigation;
+import edu.wpi.teamg.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -7,10 +9,13 @@ import javafx.fxml.FXML;
 public class LoginTopBannerController {
 
   @FXML MFXButton exitButton;
+  @FXML MFXButton logoButton;
 
   @FXML
   public void initialize() {
+
     exitButton.setOnMouseClicked(event -> exit());
+    logoButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE_SCREENSAVER_PAGE));
   }
 
   public void exit() {
