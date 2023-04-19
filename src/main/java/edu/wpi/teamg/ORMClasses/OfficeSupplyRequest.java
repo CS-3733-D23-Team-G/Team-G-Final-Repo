27d43c98cae21a5ff4f004514a8508b105efb983
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-18 20:04:06 -0400

    Adding connected Front end Methods
    Added methods for connecting into the front end, the only part missing now is the connection for submission and inserting on to the DB.

--- src/main/java/edu/wpi/teamg/ORMClasses/OfficeSupplyRequest.java
+++ src/main/java/edu/wpi/teamg/ORMClasses/OfficeSupplyRequest.java
@@ -5,28 +5,27 @@ import java.sql.Time;
 import lombok.Getter;
 import lombok.Setter;
 
-public class OfficeSupplyRequest extends Request{
-    @Getter @Setter private String supplyType;
-    @Getter @Setter private String note;
-    @Getter @Setter private String recipient;
+public class OfficeSupplyRequest extends Request {
+  @Getter @Setter private String supplyType;
+  @Getter @Setter private String note;
+  @Getter @Setter private String recipient;
 
-    public OfficeSupplyRequest(){}
+  public OfficeSupplyRequest() {}
 
-    public OfficeSupplyRequest(
-            String reqtype,
-            String empid,
-            String loc,
-            String servby,
-            StatusTypeEnum status,
-            Date deliveryDate,
-            Time deliveryTime,
-            String recipient,
-            String supplyType,
-            String note){
-        super(reqtype,empid,loc,servby,status,deliveryDate,deliveryTime);
-        this.supplyType = supplyType;
-        this.recipient=recipient;
-        this.note=note;
-    }
-
+  public OfficeSupplyRequest(
+      String reqtype,
+      String empid,
+      String loc,
+      String servby,
+      StatusTypeEnum status,
+      Date deliveryDate,
+      Time deliveryTime,
+      String recipient,
+      String supplyType,
+      String note) {
+    super(reqtype, empid, loc, servby, status, deliveryDate, deliveryTime);
+    this.supplyType = supplyType;
+    this.recipient = recipient;
+    this.note = note;
+  }
 }
