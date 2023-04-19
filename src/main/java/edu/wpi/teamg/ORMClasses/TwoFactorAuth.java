a75fa52050cf9d1f4a929c5edfca7258ea47f875
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-19 17:47:12 -0400

    Revert "Revert "TwoFactor""
    This reverts commit f99e9d1310a9d2845b1e79c66452ad55d3e87a90.

---
+++ src/main/java/edu/wpi/teamg/ORMClasses/TwoFactorAuth.java
@@ -0,0 +1,66 @@
+package edu.wpi.teamg.ORMClasses;
+
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import lombok.Getter;
+import lombok.Setter;
+import org.yaml.snakeyaml.Yaml;
+
+public class TwoFactorAuth {
+    final String mailHost = "smtp.gmail.com";
+    String sender = "brighmanwomenautosender@gmail.com";
+    @Getter @Setter String recipient;
+    int code;
+
+    public TwoFactorAuth() {
+        code = (int) (Math.random() * 900000 + 100000);
+    }
+
+    public void sendEmail() throws MessagingException {
+        String message = "Your 6 digit code is: " + code;
+        setRecipient("rganguli0753@gmail.com");
+        String password = getEmailCred().get(0);
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", mailHost);
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        props.put("mail.smtp.port", 587);
+
+        javax.mail.Session session =
+                Session.getInstance(
+                        props,
+                        new Authenticator() {
+                            @Override
+                            protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication(sender, password);
+                            }
+                        });
+
+        try {
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage.setSubject("Test Email");
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            mimeMessage.setText(message);
+            Transport.send(mimeMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<String> getEmailCred() {
+        List<String> creds = new LinkedList<>();
+        InputStream is =
+                this.getClass().getClassLoader().getResourceAsStream("edu/wpi/teamg/emailCreds.yml");
+        Yaml yaml = new Yaml();
+        Map<String, Object> data = yaml.load(is);
+        creds.add(data.get("emailPass").toString());
+        return creds;
+    }
+}
