10a1f22640961d41f3f96ffbce33f45ffbdf2718
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-20 22:30:14 -0400

    Employee Displayed
    In the welcome home page the employee name will be displayed when logging in.

--- src/main/java/edu/wpi/teamg/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamg/controllers/HomeController.java
@@ -1,9 +1,34 @@
 package edu.wpi.teamg.controllers;
 
+import edu.wpi.teamg.App;
+import edu.wpi.teamg.DAOs.EmployeeDAO;
+import edu.wpi.teamg.DBConnection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class HomeController {
 
+  @FXML Label EmpName;
+
   @FXML
-  public void initialize() {}
+  public void initialize() throws SQLException {
+    String fName = "";
+    String lName = "";
+    EmployeeDAO dao = new EmployeeDAO();
+    DBConnection db = new DBConnection();
+    db.setConnection();
+    String sql = "select * from " + dao.getTable() + " where empid = ?";
+    PreparedStatement ps = db.getConnection().prepareStatement(sql);
+    ps.setInt(1, App.employee.getEmpID());
+    ResultSet rs = ps.executeQuery();
+    while (rs.next()) {
+      fName = rs.getString("firstname");
+      lName = rs.getString("lastname");
+    }
+    db.closeConnection();
+    EmpName.setText("Welcome " + fName + " " + lName);
+  }
 }
