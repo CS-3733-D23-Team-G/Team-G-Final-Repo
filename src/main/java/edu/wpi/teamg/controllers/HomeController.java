0ec8ca72a140035327a8edd062b16ca14e261f39
Viet Hung Pham <hung.v.pham002@gmail.com>
2023-04-21 08:03:28 -0400

    display logged in user name on home page and create employee singleton on app

--- src/main/java/edu/wpi/teamg/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamg/controllers/HomeController.java
@@ -1,34 +1,17 @@
 package edu.wpi.teamg.controllers;
 
 import edu.wpi.teamg.App;
-import edu.wpi.teamg.DAOs.EmployeeDAO;
-import edu.wpi.teamg.DBConnection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class HomeController {
-
-  @FXML Label EmpName;
+  @FXML Text empName;
 
   @FXML
   public void initialize() throws SQLException {
-    String fName = "";
-    String lName = "";
-    EmployeeDAO dao = new EmployeeDAO();
-    DBConnection db = new DBConnection();
-    db.setConnection();
-    String sql = "select * from " + dao.getTable() + " where empid = ?";
-    PreparedStatement ps = db.getConnection().prepareStatement(sql);
-    ps.setInt(1, App.employee.getEmpID());
-    ResultSet rs = ps.executeQuery();
-    while (rs.next()) {
-      fName = rs.getString("firstname");
-      lName = rs.getString("lastname");
-    }
-    db.closeConnection();
-    EmpName.setText("Welcome " + fName + " " + lName);
+    empName.setText(" " + App.employee.getFirstName() + " " + App.employee.getLastName());
+    empName.setFill(Color.valueOf("#012D5A"));
   }
 }
