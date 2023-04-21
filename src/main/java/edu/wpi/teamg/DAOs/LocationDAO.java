9d02f067e1aaf704d13f2a60dd7e3c0391e0e15f
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-21 13:39:43 -0400

    Export Staus
    Exporting the service requests now is completely functional.

--- src/main/java/edu/wpi/teamg/DAOs/LocationDAO.java
+++ src/main/java/edu/wpi/teamg/DAOs/LocationDAO.java
@@ -3,7 +3,6 @@ package edu.wpi.teamg.DAOs;
 import edu.wpi.teamg.DBConnection;
 import java.io.*;
 import java.sql.*;
-import javafx.stage.FileChooser;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -75,55 +74,6 @@ public interface LocationDAO<T> extends DAO {
       br.close();
     }
 
-    db.closeConnection();
-  }
-
-  public default void exportCSV() throws SQLException, IOException {
-    db.setConnection();
-
-    Statement statement = null;
-    String sql = "select * from " + this.getTable();
-    ResultSet rs = null;
-    FileWriter writer = null;
-
-    try {
-      statement = db.getConnection().createStatement();
-      rs = statement.executeQuery(sql);
-
-      FileChooser fc = new FileChooser();
-      fc.setTitle("Save CSV Table");
-      fc.setInitialFileName(this.getTable() + ".csv");
-      File file = fc.showSaveDialog(null);
-
-      if (file != null) {
-        writer = new FileWriter(file);
-
-        int colCount = rs.getMetaData().getColumnCount();
-        for (int i = 1; i <= colCount; i++) {
-          String colLabel = rs.getMetaData().getColumnLabel(i);
-          writer.append(colLabel);
-          if (i < colCount) writer.append(",");
-        }
-        writer.append("\n");
-
-        while (rs.next()) {
-          for (int i = 1; i <= colCount; i++) {
-            String val = rs.getString(i);
-            writer.append(val);
-            if (i < colCount) writer.append(",");
-          }
-          writer.append("\n");
-        }
-      }
-
-    } catch (SQLException | IOException e) {
-      System.err.println("SQL Export error occurred");
-      e.printStackTrace();
-    }
-
-    rs.close();
-    statement.close();
-    writer.close();
     db.closeConnection();
   }
 
