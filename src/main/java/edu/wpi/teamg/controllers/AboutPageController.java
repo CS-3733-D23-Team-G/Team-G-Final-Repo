faafcfdad60fc794e01f43ee2c2ad5605a77892a
da-munchies24 <donbothermeatall16@gmail.com>
2023-04-21 16:56:29 -0400

    About page is set up.
    I still need to integrate it with the rest of the UI

---
+++ src/main/java/edu/wpi/teamg/controllers/AboutPageController.java
@@ -0,0 +1,10 @@
+package edu.wpi.teamg.controllers;
+import edu.wpi.teamg.DAOs.EdgeDAO;
+import edu.wpi.teamg.ORMClasses.Edge;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.sql.SQLException;
+import javafx.fxml.FXML;
+
+public class AboutPageController {
+}
