aae160e9904d93ae9a8a3113b96aa3fa1ff8d1d0
Rishipatelll <114689000+Rishipatelll@users.noreply.github.com>
2023-04-22 00:02:27 -0400

    employee id, need to figure out how to make the email editable and send to DB

--- src/main/java/edu/wpi/teamg/controllers/EmployeeInformation.java
+++ src/main/java/edu/wpi/teamg/controllers/EmployeeInformation.java
@@ -1,64 +1,65 @@
 package edu.wpi.teamg.controllers;
 
 import edu.wpi.teamg.App;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class EmployeeInformation {
-  @FXML Text EmployeeName;
+  @FXML
+  Text EmployeeName;
+  @FXML
+  Text EmployeeFirstName;
+  @FXML
+  Text EmployeeLastName;
+  @FXML
+  Text EmployeeEmail;
+  @FXML
+  MFXButton edit;
+  @FXML
+  MFXButton Save;
+  @FXML
+  MFXTextField EmpEmailTF;
+  @FXML
+  Text EMPID;
 
   public void initialize() {
     getEmployeeInformation();
+    edit.setOnAction(event -> editEmployeeInformation());
+    // Save.setOnMouseClicked(event -> saveEdit());
   }
 
   public void getEmployeeInformation() {
     EmployeeName.setText("" + App.employee.getFirstName() + " " + App.employee.getLastName());
+    EmployeeFirstName.setText("" + App.employee.getFirstName());
+    EmployeeLastName.setText("" + App.employee.getLastName());
+    EmployeeEmail.setText("" + App.employee.getEmail());
+    EmpEmailTF.setText("" + App.employee.getEmail());
+    EMPID.setText("" + App.employee.getEmpID());
+    // make an edit button that will allow you to edit the information
   }
-}
 
-//  private String name;
-//  private String username;
-//  private String password;
-//  private String email;
-//  private String phoneNumber;
-//  private String role;
+  public void editEmployeeInformation() {
+    EmpEmailTF.setVisible(true);
+    EmpEmailTF.setEditable(true);
+    // Save.setVisible(true);
+    edit.setVisible(false);
+    // Save.setOnAction(event -> saveEmployeeInformation());
+  }
+
+//  public void saveEdit() throws SQLException {
+//    EmployeeDAO employeeDAO = new EmployeeDAO();
+//    HashMap<Integer, Employee> emp = employeeDAO.getAll();
+//    EmpEmailTF.setVisible(false);
+//    EmpEmailTF.setEditable(false);
+//    edit.setVisible(true);
+//    // Save.setVisible(false);
+//    App.employee.setEmail(EmpEmailTF.getText());
+//    EmployeeEmail.setText("" + App.employee.getEmail());
+//    //  EmployeeDAO.update(App.employee);
 //
-//  public EmployeeInformation(
-//      String name,
-//      String username,
-//      String password,
-//      String email,
-//      String phoneNumber,
-//      String role) {
-//    this.name = name;
-//    this.username = username;
-//    this.password = password;
-//    this.email = email;
-//    this.phoneNumber = phoneNumber;
-//    this.role = role;
 //  }
-//
-//  public String getName() {
-//    return name;
-//  }
-//
-//  public String getUsername() {
-//    return username;
-//  }
-//
-//  public String getPassword() {
-//    return password;
-//  }
-//
-//  public String getEmail() {
-//    return email;
-//  }
-//
-//  public String getPhoneNumber() {
-//    return phoneNumber;
-//  }
-//
-//  public String getRole() {
-//    return role;
-//  }
-// }
+//}
+  //Me actively trying to figure out how to update the database with the new information
+}
