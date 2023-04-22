6364f16add3889258565388fbd5deb499366cde9
kristine-guan <kristineguan24@gmail.com>
2023-04-21 23:18:53 -0400

    dynamic my forms completed
    employees can now see their outstanding forms on the home page in the "my forms" section. it will display:
    request ID
    request type
    color dot to represent blank / processing

--- src/main/java/edu/wpi/teamg/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamg/controllers/HomeController.java
@@ -5,12 +5,14 @@ import static edu.wpi.teamg.DAOs.RequestDAO.getOutstandingRequest;
 import edu.wpi.teamg.App;
 import edu.wpi.teamg.DAOs.RequestDAO;
 import edu.wpi.teamg.ORMClasses.Request;
+import edu.wpi.teamg.ORMClasses.StatusTypeEnum;
 import java.sql.SQLException;
 import java.util.HashMap;
 import javafx.fxml.FXML;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 public class HomeController {
@@ -28,18 +30,71 @@ public class HomeController {
     RequestDAO requestDAO = new RequestDAO();
     HashMap<Integer, Request> hash = getOutstandingRequest(App.employee.getEmpID());
 
-    // i will eventually be number of blank and processing requests
-    for (int i = 0; i < hash.size(); i++) {
+    hash.forEach(
+        (i, m) -> {
+          Text requestID = new Text("#" + i);
+          requestID.setLayoutX(20);
+          requestID.setLayoutY(30);
+          requestID.setStyle("-fx-font-size: 24;");
+          String type = m.getReqtype();
+          String thisType = "";
 
-      Text newTest = new Text("This is VBOX test");
+          switch (type) {
+            case "M":
+              thisType = "Meal ";
+              break;
+            case "CR":
+              thisType = "Conference Room ";
+              break;
 
-      HBox newAnchorPane = new HBox();
+            case "FL":
+              thisType = "Flower ";
+              break;
+            case "FR":
+              thisType = "Furniture ";
+              break;
+            case "OS":
+              thisType = "Office Supply ";
+              break;
+          }
 
-      newAnchorPane.setStyle(
-          "-fx-background-color: #C0C0C0;-fx-background-radius: 10; -fx-pref-width: 360; -fx-pref-height: 150; fx-padding: 10; -fx-border-insets: 10; -fx-background-insets: 10;");
+          StatusTypeEnum status = m.getStatus();
+          String color = "";
+          switch (status) {
+            case blank:
+              color = "#B12B00;";
+              break;
 
-      newAnchorPane.getChildren().add(newTest);
-      forms.getChildren().add(newAnchorPane);
-    }
+            case processing:
+              color = "#0067B1;";
+              break;
+          }
+
+          Text request = new Text(thisType + "Request");
+          request.setLayoutX(15);
+          request.setLayoutY(75);
+          request.setStyle("-fx-font-size: 30;");
+
+          Circle circle = new Circle(20);
+          circle.setLayoutX(715);
+          circle.setLayoutY(65);
+          circle.setStyle("-fx-fill: " + color);
+
+          AnchorPane newAnchorPane = new AnchorPane();
+
+          newAnchorPane.setStyle(
+              "-fx-background-color: #C0C0C0;"
+                  + "-fx-background-radius: 10;"
+                  + " -fx-pref-width: 360;"
+                  + " -fx-pref-height: 150;"
+                  + " -fx-padding: 10;"
+                  + " -fx-border-insets: 10;"
+                  + " -fx-background-insets: 10;");
+
+          newAnchorPane.getChildren().add(requestID);
+          newAnchorPane.getChildren().add(request);
+          newAnchorPane.getChildren().add(circle);
+          forms.getChildren().add(newAnchorPane);
+        });
   }
 }
