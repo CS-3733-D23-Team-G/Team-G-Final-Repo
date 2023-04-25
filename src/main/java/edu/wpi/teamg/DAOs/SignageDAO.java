7bc3f365ec745db9fc15d879c83e8535a75ca73f
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-24 22:16:02 -0400

    Implement and Get
    Added the implementation of to the signage dao and also wrote its version of the get all method.

--- src/main/java/edu/wpi/teamg/DAOs/SignageDAO.java
+++ src/main/java/edu/wpi/teamg/DAOs/SignageDAO.java
@@ -1,4 +1,64 @@
 package edu.wpi.teamg.DAOs;
 
-public class SignageDAO {
+import edu.wpi.teamg.DBConnection;
+import edu.wpi.teamg.ORMClasses.Signage;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class SignageDAO implements DAO{
+    DBConnection db = new DBConnection();
+    String sql = null;
+    HashMap<Integer, Signage> signageHash = new HashMap<>();
+
+    @Override
+    public HashMap getAll() throws SQLException {
+        db.setConnection();
+
+        PreparedStatement ps;
+        ResultSet rs = null;
+        sql = "select * from" +this.getTable();
+        ps=db.getConnection().prepareStatement(sql);
+        rs=ps.executeQuery();
+
+        while(rs.next()){
+            int kiosk = rs.getInt("kiosknum");
+            Date date = rs.getDate("date");
+            String direction = rs.getString("arrow");
+
+            Signage signage = new Signage(kiosk,date,direction);
+            signageHash.put(signage.getKioskNum(),signage);
+        }
+
+        db.closeConnection();
+        return signageHash;
+    }
+
+    @Override
+    public void update(Object obj, String colName, Object obj2) throws SQLException {
+
+    }
+
+    @Override
+    public void insert(Object obj) throws SQLException {
+
+    }
+
+    @Override
+    public void delete(Object obj) throws SQLException {
+
+    }
+
+    @Override
+    public void importCSV(String path) throws SQLException {
+
+    }
+
+    @Override
+    public String getTable() {
+        return "teamgdb.iteration3.signage";
+    }
 }
