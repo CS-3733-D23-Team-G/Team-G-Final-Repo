59962744e6ad1c42b23081da13c9a811feca837a
Sebastian Baldini <sebastian.baldini@outlook.com>
2023-04-25 12:39:08 -0400

    final fixes

--- src/main/java/edu/wpi/teamg/controllers/AboutPageController.java
+++ src/main/java/edu/wpi/teamg/controllers/AboutPageController.java
@@ -1,9 +1,7 @@
 package edu.wpi.teamg.controllers;
 
-
 import edu.wpi.teamg.navigation.Navigation;
 import edu.wpi.teamg.navigation.Screen;
-
 import io.github.palexdev.materialfx.controls.MFXButton;
 import java.awt.*;
 import javafx.fxml.FXML;
