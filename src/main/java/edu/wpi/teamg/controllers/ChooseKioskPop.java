6eb0c98f41546967300f5dd91bb54dd5b439086d
Viet Hung Pham <hung.v.pham002@gmail.com>
2023-04-25 22:40:54 -0400

    About page and credits

--- src/main/java/edu/wpi/teamg/controllers/ChooseKioskPop.java
+++ src/main/java/edu/wpi/teamg/controllers/ChooseKioskPop.java
@@ -5,16 +5,16 @@ import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 
 public class ChooseKioskPop {
-    @FXML MFXButton setKioskOne;
-    @FXML MFXButton setKioskTwo;
+  @FXML MFXButton setKioskOne;
+  @FXML MFXButton setKioskTwo;
 
-    @FXML
-    public void initialize(){
-        setKioskOne.setOnAction(event -> setKiosk(1));
-        setKioskTwo.setOnAction(event -> setKiosk(2));
-    }
+  @FXML
+  public void initialize() {
+    setKioskOne.setOnAction(event -> setKiosk(1));
+    setKioskTwo.setOnAction(event -> setKiosk(2));
+  }
 
-    public void setKiosk(int i){
-        App.setKioskNumber(i);
-    }
+  public void setKiosk(int i) {
+    App.setKioskNumber(i);
+  }
 }
