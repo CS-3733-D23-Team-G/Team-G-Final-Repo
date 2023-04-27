a375b3c11543fcf87d09d0b2fd83f6046aa5674b
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-27 12:14:00 -0400

    Refactored for object
    Refactored the tts to be an object that can be both paused and started.

--- src/main/java/edu/wpi/teamg/ORMClasses/TextToSpeech.java
+++ src/main/java/edu/wpi/teamg/ORMClasses/TextToSpeech.java
@@ -8,19 +8,28 @@ import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerModeDesc;
 
 public class TextToSpeech {
-  public static void main(String[] args)
-      throws EngineException, AudioException, InterruptedException {
+  String spoken;
+  Synthesizer synth = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
+  public TextToSpeech(String spoken)throws EngineException {
     System.setProperty(
-        "freetts.voices", "com.sun.speech.freetts.en.us" + ".cmu_us_kal.KevinVoiceDirectory");
+            "freetts.voices", "com.sun.speech.freetts.en.us" + ".cmu_us_kal.KevinVoiceDirectory");
     Central.registerEngineCentral("com.sun.speech.freetts" + ".jsapi.FreeTTSEngineCentral");
-
     Synthesizer synth = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
     synth.allocate();
-
+    this.spoken=spoken;
+  }
+  public void resumeSpeech() throws AudioException {
     synth.resume();
-    synth.speakPlainText("This is a test in the speech recognition software", null);
+  }
+  public void speakText() throws InterruptedException, EngineException {
+    synth.speakPlainText(
+            spoken,
+            null);
     synth.waitEngineState(Synthesizer.QUEUE_EMPTY);
-
     synth.deallocate();
+  }
+  
+  public void pauseSpeech(){
+    synth.pause();
   }
 }
