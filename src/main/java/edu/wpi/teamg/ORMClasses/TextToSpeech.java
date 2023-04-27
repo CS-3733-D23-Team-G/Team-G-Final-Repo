6a688f6c1667efafd2c3ac4517943e3fbd326085
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-27 12:23:09 -0400

    Fixed bugs
    Had to restructure so that the tts can actually speak.

--- src/main/java/edu/wpi/teamg/ORMClasses/TextToSpeech.java
+++ src/main/java/edu/wpi/teamg/ORMClasses/TextToSpeech.java
@@ -10,26 +10,25 @@ import javax.speech.synthesis.SynthesizerModeDesc;
 public class TextToSpeech {
   String spoken;
   Synthesizer synth = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
-  public TextToSpeech(String spoken)throws EngineException {
+
+  public TextToSpeech(String spoken) throws EngineException, AudioException, InterruptedException {
+    this.spoken = spoken;
     System.setProperty(
-            "freetts.voices", "com.sun.speech.freetts.en.us" + ".cmu_us_kal.KevinVoiceDirectory");
+        "freetts.voices", "com.sun.speech.freetts.en.us" + ".cmu_us_kal.KevinVoiceDirectory");
     Central.registerEngineCentral("com.sun.speech.freetts" + ".jsapi.FreeTTSEngineCentral");
     Synthesizer synth = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
     synth.allocate();
-    this.spoken=spoken;
-  }
-  public void resumeSpeech() throws AudioException {
     synth.resume();
-  }
-  public void speakText() throws InterruptedException, EngineException {
-    synth.speakPlainText(
-            spoken,
-            null);
+    synth.speakPlainText(spoken, null);
     synth.waitEngineState(Synthesizer.QUEUE_EMPTY);
     synth.deallocate();
   }
-  
-  public void pauseSpeech(){
+
+  public void resumeSpeech() throws AudioException {
+    synth.resume();
+  }
+
+  public void pauseSpeech() {
     synth.pause();
   }
 }
