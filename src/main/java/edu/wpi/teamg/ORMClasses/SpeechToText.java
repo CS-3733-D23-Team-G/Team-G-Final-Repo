a05aa8a8c1bab9b6406bdfa5ce5fb2592223090a
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-28 15:03:00 -0400

    Wrote object version
    Wrote a refactoring version of it that should make the speech commands be time based.

--- src/main/java/edu/wpi/teamg/ORMClasses/SpeechToText.java
+++ src/main/java/edu/wpi/teamg/ORMClasses/SpeechToText.java
@@ -4,8 +4,53 @@ import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import edu.cmu.sphinx.api.SpeechResult;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class SpeechToText {
+  Configuration config = new Configuration();
+  String command = "";
+  private boolean stopped = false;
+  private long timeLim;
+  private ScheduledExecutorService executorService;
+  LiveSpeechRecognizer recog;
+
+
+  public SpeechToText() throws IOException {
+    config.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+    config.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+    config.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+    executorService = Executors.newSingleThreadScheduledExecutor();
+    recog = new LiveSpeechRecognizer(config);
+  }
+
+  public void detectCommand(int duration) throws IOException {
+    try{
+      this.timeLim = duration;
+      recog.startRecognition(true);
+
+      executorService.schedule(this::stop,timeLim, TimeUnit.SECONDS);
+      SpeechResult result;
+      while(!stopped&&(result=recog.getResult())!=null){
+        command = result.getHypothesis();
+        System.out.println("The command was: "+command);
+      }
+    }catch(Exception e){
+      e.printStackTrace();
+    }finally {
+      if(recog!=null){
+        recog.stopRecognition();
+      }
+      executorService.shutdownNow();
+    }
+  }
+
+  private void stop(){
+    recog.stopRecognition();
+    stopped=true;
+  }
+
   public static void main(String[] args) throws IOException {
     Configuration config = new Configuration();
 
